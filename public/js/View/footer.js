67586d44d3b06305590ba9fdc39e7775338ff946
Felipe Hashimoto <felipe.hashi@gmail.com>
2019-04-30 10:19:32 -0300

    Create footer.js
    criação do footer e seus elementos

---
+++ public/js/View/footer.js
@@ -0,0 +1,34 @@
+const texto_footer = `
+  <div class="border-top border-secundary w-100 mt-4 p-2 text-uppercase"> 
+    <span class="font-weight-bolder">
+      <span style="color: #234562">cheg&#64;</span> 
+      <span style="color: #f8b152;">disso</span>
+    </span>
+
+    <span>
+      política de direitos autorais
+    </span>
+    <span>
+      sobre
+    </span>
+    <span>
+      equipe
+    </span>
+    <span>
+      contato
+    </span>
+    <span>
+      acompanhe:
+      <a href="https://www.facebook.com/blog.chegadisso/" target="_blank">
+        <i class="fab fa-facebook-f px-2 text-dark"></i>
+      </a>
+      <a href="https://www.instagram.com/chega.disso/" target="_blank">
+        <i class="fab fa-instagram text-dark"></i>
+      </a>
+    </span>
+  </div>
+`;
+
+const final_footer = texto_footer;
+
+$("#footer").html(final_footer);
