312a656d76ccb3f0971181c1ada867e7856fc722
Felipe Hashimoto <felipe.hashi@gmail.com>
2019-04-30 12:14:31 -0300

    Update footer.js
    criação do footer

--- public/js/View/footer.js
+++ public/js/View/footer.js
@@ -1,31 +1,44 @@
 const texto_footer = `
-  <div class="border-top border-secundary w-100 mt-4 p-2 text-uppercase"> 
-    <span class="font-weight-bolder">
-      <span style="color: #234562">cheg&#64;</span> 
-      <span style="color: #f8b152;">disso</span>
-    </span>
+  <div class="border-top border-secundary w-100 mt-4 p-2 text-uppercase container-fluid"> 
+    <div class="row ">
 
-    <span>
-      política de direitos autorais
-    </span>
-    <span>
-      sobre
-    </span>
-    <span>
-      equipe
-    </span>
-    <span>
-      contato
-    </span>
-    <span>
-      acompanhe:
-      <a href="https://www.facebook.com/blog.chegadisso/" target="_blank">
-        <i class="fab fa-facebook-f px-2 text-dark"></i>
-      </a>
-      <a href="https://www.instagram.com/chega.disso/" target="_blank">
-        <i class="fab fa-instagram text-dark"></i>
-      </a>
-    </span>
+      <div class="col-sm-1"></div>
+
+      <div class="col-sm-2 pr-0 text-center">
+        <span class="font-weight-bolder" style="font-size:1.5em">
+          <span style="color: #234562">cheg&#64;</span> 
+          <span style="color: #f8b152;">disso</span>
+        </span>
+      </div>
+
+      <div class="col-sm-6 font-weight-bold" style="font-size:.8em; color: #234562">
+        <div class="container h-100 ">
+          <div class="row h-100 justify-content-center">
+            <div class="pr-3 pt-2">
+              política de direitos autorais
+            </div>
+            <div class="pr-3 pt-2">
+              sobre
+            </div>
+            <div class="pr-3 pt-2">
+              equipe
+            </div>
+            <div class="pr-3 pt-2">
+              contato
+            </div>
+            <div class="pr-3 pt-2">
+              acompanhe:
+              <a href="https://www.facebook.com/blog.chegadisso/" target="_blank">
+                <i class="fab fa-facebook-f px-2 text-dark"></i>
+              </a>
+              <a href="https://www.instagram.com/chega.disso/" target="_blank">
+                <i class="fab fa-instagram text-dark"></i>
+              </a>
+            </div>
+          </div>
+        <div>
+      </div>
+    </div>
   </div>
 `;
 
